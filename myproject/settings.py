97d3a540c0d958a5d7a01ef5d977202fdebc5be2
Diwash <diwashshrestha108@gmail.com>
2025-02-09 13:33:50 +0545

    allowed hosts

--- myproject/settings.py
+++ myproject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-+&s%*ielra^y^*$t86rxzvpe995)+&#^l=wcg+ft@ma1i57zo(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['board-project.onrender.com',127.0.0.1]
+ALLOWED_HOSTS = ['board-project.onrender.com','127.0.0.1',]
 
 
 # Application definition
